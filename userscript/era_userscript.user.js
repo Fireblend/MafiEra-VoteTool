afd5e4667ba57d0e294b0f9bb69bc6cbbc5a8b13
Sergio Morales Esquivel <sergiome@gmail.com>
2018-03-14 13:54:35 -0600

    Update era_userscript.user.js

--- userscript/era_userscript.user.js
+++ userscript/era_userscript.user.js
@@ -1,61 +1,65 @@
 // ==UserScript==
 // @name        MafiEraVoteTool
-// @version     0.1
+// @version     0.2
 // @namespace   http://www.fireblend.com/
 // @updateURL   https://openuserjs.org/meta/Fireblend/era_userscript.meta.js
 // @downloadURL https://openuserjs.org/src/scripts/Fireblend/era_userscript.user.js
-// @license     CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
-// @icon        https://dl.dropboxusercontent.com/u/57161259/icons/cs-ohnoes-icon.png
+// @license     MIT
+// @icon        https://cdn.discordapp.com/icons/161525734102401024/d15bc9248dcbe0d35dbbba1fa096c348.png
 // @homepageURL http://www.fireblend.com/
 // @author      Sergio Morales
 // @description The MafiEra Vote Tool, made easier!
 // @include     http*://www.resetera.com/threads/*
 // @include     http*://www.resetera.com/threads/*/*
-// @require     http://code.jquery.com/jquery-1.10.2.js
+// @require     https://code.jquery.com/ui/1.12.1/jquery-ui.js
 // @require     http://code.jquery.com/ui/1.11.4/jquery-ui.js
+// @resource    customCSS https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css
 // @run-at      document-idle
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
 // ==/UserScript==
 
+(function () {
+  'use strict';
 
-(function() {
-    'use strict';
+  function addButton() {
+    var buttonLocations = document.querySelectorAll('fieldset.breadcrumb');
+    var a = makeButton();
+    buttonLocations[0].appendChild(a);
+    var b = makeButton();
+    buttonLocations[2].appendChild(b);
+    for (var location in buttonLocations) {}
+  }
 
-    function addButton() {
-        var buttonLocations = document.querySelectorAll('fieldset.breadcrumb');
-          var a = makeButton();
-          buttonLocations[0].appendChild(a);
-          var b = makeButton();
-          buttonLocations[2].appendChild(b);
-        for (var location in buttonLocations){
-        }
-    }
-
-function openDialog() {
+  function openDialog() {
     var url = window.location.href;
-    var page = "https://vote.fireblend.com/"+url.split("/threads/")[1].split("/")[0];
+    var page = "https://vote.fireblend.com/" + url.split("/threads/")[1].split("/")[0] + "/simple";
 
     var $dialog = $('<div></div>')
-    .html('<iframe style="border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
-    .dialog({
+      .html('<iframe id="myDialog" style="background-color: #ffffff; border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
+      .dialog({
         autoOpen: false,
         modal: true,
-        height: 625,
-        width: 900
-    });
+        height: 700,
+        width: 500,
+        title: "MafiEra Vote Count",
+        resizable: false
+      });
     $dialog.dialog('open');
-}
+  }
 
-    function makeButton() {
-        var a = document.createElement('a');
-        var url = window.location.href;
-        a.setAttribute('href', "javascript:openDialog()");
-        a.setAttribute('class', "crumb bottomLink videob");
-        a.addEventListener("click", openDialog, false);
-
-
-        a.setAttribute('target', '_blank');
-        a.appendChild(document.createTextNode("Vote Count"));
-        return a;
-    }
-    addButton();
+  function makeButton() {
+    var a = document.createElement('span');
+    var url = window.location.href;
+    a.setAttribute('style', 'cursor:pointer;');
+    a.setAttribute('class', "crumb bottomLink videob");
+    a.addEventListener("click", openDialog, false);
+    a.setAttribute('target', '_blank');
+    a.appendChild(document.createTextNode("Vote Count"));
+    return a;
+  }
+  var newCSS = GM_getResourceText("customCSS");
+  GM_addStyle(newCSS);
+  GM_addStyle('.ui-dialog .ui-dialog-content { overflow: hidden; }');
+  addButton();
 })();
